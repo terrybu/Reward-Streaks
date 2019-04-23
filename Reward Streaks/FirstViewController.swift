84318506c1e45645e37a8d65708fe2d2b78c6cb3
Terry Bu <terrybu88@gmail.com>
2019-04-23 13:25:11 -0400

    Initial Commit

---
+++ Reward Streaks/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Reward Streaks
+//
+//  Created by Terry Bu on 4/23/19.
+//  Copyright © 2019 Terry Bu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Reward Streaks/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Reward Streaks
+//
+//  Created by Terry Bu on 4/23/19.
+//  Copyright © 2019 Terry Bu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
